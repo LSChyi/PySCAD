4d6aa5910b9edfe78c1e757c88487865127a160f
LSChyi <alan81920@gmail.com>
2019-08-31 00:24:13 +0800

    add translate

---
+++ objects/translate.py
@@ -0,0 +1,21 @@
+from .util import *
+from . import node
+
+class translate(node.Node):
+    def __init__(self,  vector, targets, *args):
+        if not isVector(vector):
+            raise Exception('invalid vector: {vector}')
+        self.x, self.y, self.z = vector
+
+        self.children = []
+        if type(targets) != list:
+            self.children.append(targets)
+        else:
+            self.children = targets
+        self.children += args
+
+    def transcript(self):
+        childrenStr = ''.join([ child.transcript() for child in self.children ])
+        return f'translate([{self.x}, {self.y}, {self.z}]){{{childrenStr}}}'
+
+node.transformations['translate'] = translate
